807c37ef8113c1d8ee440aa5f0c27939c1ce4e30
sni10 <9530163+sni10@users.noreply.github.com>
2025-11-25 11:08:49 +0200

    test: add unit test for FinderProductsService execute method

--- tests/Unit/Service/FinderProductsServiceTest.php
+++ tests/Unit/Service/FinderProductsServiceTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Unit\Service;
 
 use App\Dto\KeepaInputDto;
+use App\Dto\KeepaOutputDto;
 use App\Service\FinderProductsService;
 use App\Service\KeepaService;
 use App\Service\Validator\MessageValidator;
@@ -85,6 +86,85 @@ class FinderProductsServiceTest extends TestCase
                 'tokensUsed' => 0,
                 'pages' => 0,
                 'sleepSeconds' => 0,
+            ],
+            $result,
+        );
+    }
+
+    public function testExecuteDispatchesValidOutputsAndReturnsMappedStats(): void
+    {
+        $inputDto = KeepaInputDto::fromArray([
+            'id' => 2,
+            'domain_id' => 3,
+            'brand' => 'nike',
+            'time_from' => '2025-02-01',
+            'time_to' => '2025-02-28',
+            'version' => 1,
+            'status' => 'IN_PROGRESS',
+            'step' => 1,
+        ]);
+
+        $validatedInput = clone $inputDto;
+
+        $this->messageValidator
+            ->expects(self::once())
+            ->method('validateInput')
+            ->with($inputDto)
+            ->willReturn($validatedInput);
+
+        $product = new KeepaOutputDto();
+        $product->asin = 'B000TEST00';
+
+        $generator = (function () use ($product) {
+            yield $product;
+        })();
+
+        $this->keepaService
+            ->expects(self::once())
+            ->method('execute')
+            ->with($validatedInput)
+            ->willReturn($generator);
+
+        $validatedOutput = clone $product;
+
+        $this->messageValidator
+            ->expects(self::once())
+            ->method('validateOutput')
+            ->with($product)
+            ->willReturn($validatedOutput);
+
+        $this->bus
+            ->expects(self::once())
+            ->method('dispatch')
+            ->with($validatedOutput);
+
+        $this->keepaService
+            ->expects(self::once())
+            ->method('getStats')
+            ->willReturn([
+                'asinsFound' => 5,
+                'tokensUsed' => 42,
+                'pagesProcessed' => 3,
+                'sleepSeconds' => 7,
+                'tokensLeft' => 100,
+            ]);
+
+        $service = new FinderProductsService(
+            $this->logger,
+            $this->keepaService,
+            $this->messageValidator,
+            $this->bus,
+        );
+
+        $result = $service->execute($inputDto);
+
+        self::assertSame(
+            [
+                'asinsFound' => 5,
+                'tokensUsed' => 42,
+                'pages' => 3,
+                'sleepSeconds' => 7,
+                'tokensLeft' => 100,
             ],
             $result,
         );
